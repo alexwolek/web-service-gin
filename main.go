48ac514d33ea4e0f7147f8960e99d9ae9fccded7
Alex Wolek <alex.wolek@gmail.com>
2022-03-15 11:10:22 -0500

    adding album controllers file

--- main.go
+++ main.go
@@ -1,30 +1,15 @@
 package main
 
 import (
-	"example/web-service-gin/models"
-
-	"net/http"
+	"example/web-service-gin/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
-// albums slice to seed record album data.
-var albums = []models.Album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
 func main() {
 	router := gin.Default()
 
-	albums := router.Group("/albums")
-	albums.GET("", getAlbums)
+	controllers.AddAlbumsController(router)
 
 	router.Run("localhost:8080")
 }
