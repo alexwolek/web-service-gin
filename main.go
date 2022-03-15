cc409c6d0ef03aaa3139db8e33b2cd9f3f817ef1
Alex Wolek <alex.wolek@gmail.com>
2022-03-15 10:51:47 -0500

    adding models directory

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
+	"example/web-service-gin/models"
+
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
 // albums slice to seed record album data.
-var albums = []album{
+var albums = []models.Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
@@ -28,7 +22,9 @@ func getAlbums(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+
+	albums := router.Group("/albums")
+	albums.GET("", getAlbums)
 
 	router.Run("localhost:8080")
 }
