be3f9a6d15fffcb968c83e86f852e6224e8910a0
Alex Wolek <alex.wolek@gmail.com>
2022-03-16 21:55:24 -0500

    minor refactoring

--- controllers/albums.go
+++ controllers/albums.go
@@ -16,15 +16,10 @@ func AddAlbumsController(engine *gin.Engine) {
 	}
 }
 
-// albums slice to seed record album data.
-var albums = []models.Album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 // getAllAlbums responds with the list of all albums as JSON.
 func getAllAlbums(c *gin.Context) {
+	albums := models.GetAllAlbums()
+
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
@@ -32,29 +27,24 @@ func getAllAlbums(c *gin.Context) {
 // parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
-
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	album := models.GetAlbumByID(id)
+	if album == nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	} else {
+		c.IndentedJSON(http.StatusOK, album)
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 // addAlbum adds an album from JSON received in the request body.
 func addAlbum(c *gin.Context) {
 	var newAlbum models.Album
 
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
+	// Call BindJSON to bind the received JSON to newAlbum.
 	if err := c.BindJSON(&newAlbum); err != nil {
 		return
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
+	// Add the new album to the collection.
+	models.AddNewAlbum(newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
--- models/album.go
+++ models/album.go
@@ -7,3 +7,33 @@ type Album struct {
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
+
+// albums slice to seed record album data.
+var albums = []Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// returns all albums in the collection
+func GetAllAlbums() []Album {
+	return albums
+}
+
+// GetAlbumByID locates the album whose ID value matches the id
+func GetAlbumByID(id string) *Album {
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			return &a
+		}
+	}
+
+	return nil
+}
+
+// adds a new album to the collection
+func AddNewAlbum(newAlbum Album) {
+	albums = append(albums, newAlbum)
+}
